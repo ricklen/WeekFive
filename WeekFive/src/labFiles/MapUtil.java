4db54f69a17741361ac47c70d401117ff629f4d9
Jaap Eijsker <jaapeijsker@gmail.com>
2016-12-16 13:49:26 +0100

    Nog steeds bezig met Map

--- WeekFive/src/labFiles/MapUtil.java
+++ WeekFive/src/labFiles/MapUtil.java
@@ -1,19 +1,17 @@
 package labFiles;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapUtil {
-	// TODO: ask these questions
-	//@ requires dat er een goede map in gaat?
-	//@ ensures \result == true || \result == false;
-	//@ ensures duplicate value ==> false; 			???
-	//*@pure*/
+	
+	//@ ensures (\forall k1,k2; k1 != k2; get(k1) != get(k2));
+	//@pure
 	// This method puts all values into a set to check for duplicates
     public static <K, V> boolean isOneOnOne(Map<K, V> map) {
-        // Waarom moet deze static? ???
     	int length = map.size();
     	Set<V> set = new HashSet<V>(map.values());
     	if (set.size() < length) {
@@ -22,38 +20,32 @@ public class MapUtil {
     		return true;
     	}
 
-    }
+    } 
 
-    public static <K, V> 
-           boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
-        int counter = 0;
-        
-//        for (int i = 0; i < range.size(); i++) {
-//        	if (!map.containsKey(range[i])) {
-//        		
-//        	}
-//        }
-        
-        
-        
-    	for (int i = 0; i < map.size(); i++) {
-    		Object x = map.get(i);
-    		for(Object c : range){
-    			if (x.equals(c)){
-    				counter++;
-    			}
-    			
-    		}
-    	}
-    	if (counter == range.size()){
-    		return true;
-    	}
-    	
-    	// kijk naar alle values in de range of er een key bij hoort waarvoor geldt k.get = v
-    	
-        return false;
+    //@ ensures (\forall v; range.contains(v); map.containsValue(v)); 
+    public static <K, V> boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
+        for (V i : range) {
+        	if (!map.containsValue(i)) {
+        		return false;
+        	}
+        }
+        return true;
     }
+        
     public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
+    	Map<V, Set<K>> inverse = new HashMap<V, Set<K>>();
+    	
+    	ArrayList<V> list = new ArrayList<V>(map.values());
+    	Set<K> set = new HashSet<K>(map.keySet());
+    	
+    	for (V v : list) {
+    		inverse.put(v, set.
+    		
+    		
+    	}
+    	
+    	
+    	
         // TODO: implement, see exercise P-5.3
         return null;
 	}
